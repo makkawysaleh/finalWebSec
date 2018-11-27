68aec23f02e43a2958028aefc0110fd4737576c1
makkawy work <makkawywork@salehs-MacBook-Pro.local>
2018-11-26 20:19:51 -0500

    add route comment a post by id.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -152,4 +152,41 @@ router.post(
   }
 );
 
+// @route   POST api/posts/comment/:id
+// @desc    Add comment to post
+// @access  Private
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // If any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.body.id
+        };
+
+        // Add to comments array
+        post.comment.unshift(newComment);
+
+        // Save
+        post.save().then(post => res.json(post));
+      })
+      .catch(err =>
+        ers.status(404).json({
+          postnotfound: 'No post found!'
+        })
+      );
+  }
+);
 module.exports = router;
