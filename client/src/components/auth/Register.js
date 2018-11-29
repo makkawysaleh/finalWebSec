32b57fcd35520d557e21830c0c8fc0f66cda562f
makkawy work <makkawywork@host10-7-72-57.vpn.udel.edu>
2018-11-29 09:34:50 -0500

    adding reducer in authAction

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import classnames from 'classnames';
-
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions';
 class Register extends Component {
   constructor() {
     super();
@@ -29,11 +30,11 @@ class Register extends Component {
       password: this.state.password,
       password2: this.state.password2
     };
-
-    axios
-      .post('/api/users/register', newUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({ errors: err.response.data }));
+    this.props.registerUser(newUser);
+    //  axios
+    //    .post('/api/users/register', newUser)
+    //    .then(res => console.log(res.data))
+    //    .catch(err => this.setState({ errors: err.response.data }));
   }
 
   render() {
@@ -124,4 +125,7 @@ class Register extends Component {
     );
   }
 }
-export default Register;
+export default connect(
+  null,
+  { registerUser }
+)(Register);
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,18 @@
+import { TEST_DISPATCH } from '../actions/types';
+
+const initialState = {
+  isAuthenicated: false,
+  userL: {}
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case TEST_DISPATCH:
+      return {
+        ...state,
+        user: action.payload
+      };
+    default:
+      return state;
+  }
+}
